e7e694fc7c698acafa35617370523f840c3e11b1
Prachi <gupta.prachi39@gmail.com>
2021-03-09 22:56:37 +0530

    exponential backoff for retry

--- service.go
+++ service.go
@@ -1,20 +1,58 @@
 package main
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"golang.org/x/text/language"
+)
 
 // Service is a Translator user.
 type Service struct {
-	translator Translator
+	translator MyTranslator
 }
+
+type MyTranslator struct {
+	myTranslator Translator
+}
+
+var N = 10 //No of retries
+// var N = os.Args[1]
 
 func NewService() *Service {
 	t := newRandomTranslator(
 		100*time.Millisecond,
 		500*time.Millisecond,
-		0.1,
+		0.8,
 	)
 
-	return &Service{
-		translator: t,
+	a := &MyTranslator{
+		myTranslator: t,
 	}
+
+	return &Service{
+		translator: *a,
+	}
+}
+
+// Mocking the external translation
+func (m *MyTranslator) Translate(ctx context.Context, from, to language.Tag, data string) (string, error) {
+	var res string
+	var err error
+	var duration = 2
+	for N > 0 {
+		res, err = m.myTranslator.Translate(ctx, from, to, data)
+		if err != nil {
+			N--
+			waitTime := time.Duration(duration) * time.Millisecond
+			time.Sleep(waitTime)
+			fmt.Println("Error Occured, Waiting after ", waitTime)
+			duration = duration * 2
+			continue
+		} else {
+			return res, err
+		}
+	}
+	return res, err
 }
